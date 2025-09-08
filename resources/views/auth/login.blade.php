6a3f3cc008784ff4e6eb72085f659bb89d805904
Loldeluis <luismaldonadourib@gmail.com>
2025-09-08 11:52:49 -0500

    register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,6 +42,15 @@
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
             </x-primary-button>
+
+                        @if (Route::has('register'))
+                <a href="{{ route('register') }}"
+                    class="ml-4 inline-flex items-center px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    {{ __('Register') }}
+                </a>
+            @endif
+
+
         </div>
     </form>
 </x-guest-layout>
